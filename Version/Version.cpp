4e4f5cef628ea27b043f6a52d2c7373b6506c0ba
Moong <moongstory@gmail.com>
2022-12-04 23:04:09 +0900

    split 부분 StringTool 클래스 사용

--- Version/Version.cpp
+++ Version/Version.cpp
@@ -1,65 +1,32 @@
 #include "Version.h"
 
-// https://github.com/MoongStory/FileInfo
 #include "../../FileInfo/FileInfo/FileInfo.h"
+
+#include "../../ConvertDataType/ConvertDataType/ConvertDataType.h"
+
+#include "../../StringTool/StringTool/StringTool.h"
 
 #include <vector>
 
 int MOONG::Version::Compare(std::string param_version1, std::string param_version2, std::string delimiters/* = ".,"*/)
 {
+	std::vector<std::string> version_temp;
+
+	version_temp = MOONG::StringTool::split(param_version1, delimiters);
+
 	std::vector<int> version1;
+	for (size_t i = 0; i < version_temp.size(); i++)
+	{
+		version1.push_back(MOONG::ConvertDataType::string_to_integer(version_temp.at(i)));
+	}
+
+	version_temp = MOONG::StringTool::split(param_version2, delimiters);
+
 	std::vector<int> version2;
-
-	char* token = NULL;
-#if _MSC_VER > 1200
-	char* next_token = NULL;
-
-	token = strtok_s((char*)(param_version1.c_str()), delimiters.c_str(), &next_token);
-
-	while (token != NULL)
+	for (size_t i = 0; i < version_temp.size(); i++)
 	{
-		// Get next token:
-		if (token != NULL)
-		{
-			version1.push_back(atoi(token));
-
-			token = strtok_s(NULL, delimiters.c_str(), &next_token);
-		}
+		version2.push_back(MOONG::ConvertDataType::string_to_integer(version_temp.at(i)));
 	}
-
-	token = strtok_s((char*)(param_version2.c_str()), delimiters.c_str(), &next_token);
-
-	while (token != NULL)
-	{
-		// Get next token:
-		if (token != NULL)
-		{
-			version2.push_back(atoi(token));
-
-			token = strtok_s(NULL, delimiters.c_str(), &next_token);
-		}
-	}
-#else
-	token = strtok((char*)(param_version1.c_str()), delimiters.c_str());
-
-	while (token != NULL)
-	{
-		version1.push_back(atoi(token));
-
-		// Get next token:
-		token = strtok(NULL, delimiters.c_str()); // C4996
-	}
-
-	token = strtok((char*)(param_version2.c_str()), delimiters.c_str());
-
-	while (token != NULL)
-	{
-		version2.push_back(atoi(token));
-
-		// Get next token:
-		token = strtok(NULL, delimiters.c_str()); // C4996
-	}
-#endif
 
 	size_t i = 0;
 	while (true)
